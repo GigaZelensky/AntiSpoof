baf4ac7132d5b87d014fe79979b324798a9653c6
GigaZelensky <tomasvenrura13@gmail.com>
2025-06-16 02:31:27 +0100

    Update AlertManager.java

--- src/main/java/com/gigazelensky/antispoof/managers/AlertManager.java
+++ src/main/java/com/gigazelensky/antispoof/managers/AlertManager.java
@@ -21,7 +21,7 @@ public class AlertManager {
     private final Set<UUID> playersWithAlertPermission = ConcurrentHashMap.newKeySet();
     
     // Alert cooldown in milliseconds (3 seconds by default)
-    private static final long ALERT_COOLDOWN = 1;
+    private static final long ALERT_COOLDOWN = 3;
     
     public AlertManager(AntiSpoofPlugin plugin) {
         this.plugin = plugin;
